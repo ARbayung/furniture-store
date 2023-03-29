34cba76c2a079548a21104c34d48747209f7e276
Alice Angelica <aliceakatinas@hotmail.com>
2023-03-29 21:09:25 +0100

    added footer.js and footerStyle.js, modified app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Results from "./components/Results-page";
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Results/>
+
+      <Footer />
     </div>
+
   );
 }
 
