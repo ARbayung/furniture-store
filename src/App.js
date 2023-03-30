90f9c75b48ed66893d84f917def8f73fa6521e08
adammbnitro <adambenkada123@gmail.com>
2023-03-30 19:31:11 +0100

    added navbar to application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Results from "./components/Results-page";
 import Footer from './components/Footer';
+import Navbar from "./components/Navbar"
+import './components/Navbar.css'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Results/>
 
       <Footer />
