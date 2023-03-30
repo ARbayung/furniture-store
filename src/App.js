3617639d56be883b2ebe74cd849a423ddd219c12
Des <112896886+giorhodes@users.noreply.github.com>
2023-03-30 18:12:25 +0100

    body version 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Results from "./components/Results-page";
 import './App.css';
+import Body from "./components/body";
 
 function App() {
   return (
     <div className="App">
       <Results/>
+      <Body/>
     </div>
   );
 }
